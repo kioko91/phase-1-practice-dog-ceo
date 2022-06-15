70bfa4a3a89e1dbe16d97892a064a68af8704589
Calvin <calvinkioko91@gmail.com>
2022-06-15 15:47:31 +0300

    commit tree 6e05b33466df3c6956a9ac866eee81f727dfb576

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function()
+{
+    fetchDogImages()
+})
+
+function fetchDogImages(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    
+    .then(data => {
+        console.log(data.message);
+        data.message.forEach(image => loadImagestoDom(image))});
+
+}
+function loadImagestoDom(image){
+    let dogsCont = document.getElementById('dog-image-container');
+   let ourImage =document.createElement('img');
+   ourImage.src = image;
+   dogsCont.appendChild(ourImage);
+
+}
